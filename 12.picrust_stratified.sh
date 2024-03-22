81c244797ff1961b841cf00c7b6e71259f20df07
Erica Ryu <ericapryu@gmail.com>
2024-03-22 11:09:05 -0400

    fixed all files for fixed phyloseq

---
+++ 12.picrust_stratified.sh
@@ -0,0 +1,83 @@
+#!/bin/bash 
+
+# The purpose of this script is to run the stratified module of picrust on the qiagen samples - takes a few hours total, especially the pathway abundance inference step
+
+#SBATCH --time=12:00:00  
+#SBATCH --nodes=2
+#SBATCH --mem=10gb
+#SBATCH -o picrust_stratified_qiagen_v3.txt  
+#SBATCH -A exd44 
+
+d1=$(date +%s) 
+
+RUN_PATH="/storage/group/exd44/default/epr5208/Himalayan_oral_microbiome/picrust2/v3"
+
+BIOM="picrust_seq_tab_qiagen"
+SEQ="picrust_seq_qiagen"
+OUT_DIR="picrust2_qiagen_output"
+KEGG_MAP="ko_to_path"
+KEGG_NAME="ko_names"
+
+# change to directory that has the files
+cd $RUN_PATH
+
+pwd 
+echo "Job started"
+
+# load conda environment
+module load anaconda3
+source activate picrust2
+
+# make directory for output
+mkdir $OUT_DIR
+
+# format sequence file
+grep -v '^x' ${SEQ}.txt | awk '{print ">seq"$1"\n"$2}' > ${SEQ}.fna
+
+# sequence placement
+place_seqs.py -s ${SEQ}.fna -o ${OUT_DIR}/out.tre -p 1 --intermediate placement_working
+
+# hidden state prediction
+hsp.py -i 16S -t ${OUT_DIR}/out.tre -o ${OUT_DIR}/marker_nsti_predicted.tsv.gz -p 1 -n
+
+hsp.py -i EC -t ${OUT_DIR}/out.tre -o ${OUT_DIR}/EC_predicted.tsv.gz -p 1
+
+hsp.py -i KO -t ${OUT_DIR}/out.tre -o ${OUT_DIR}/KO_predicted.tsv.gz -p 1
+
+# metagenome prediction
+metagenome_pipeline.py -i ${BIOM}.biom \
+                       -m ${OUT_DIR}/marker_nsti_predicted.tsv.gz \
+                       -f ${OUT_DIR}/KO_predicted.tsv.gz \
+                       -o ${OUT_DIR}/KO_metagenome_out_stratified \
+                       --strat_out
+
+# infer pathway abundances
+pathway_pipeline.py -i ${OUT_DIR}/KO_metagenome_out_stratified/pred_metagenome_contrib.tsv.gz \
+                    -o ${OUT_DIR}/KEGG_pathways_out_stratified \
+                    --no_regroup \
+                    --map ${KEGG_MAP}.tsv \
+                    --per_sequence_contrib \
+                    --per_sequence_abun ${OUT_DIR}/KO_metagenome_out_stratified/seqtab_norm.tsv.gz \
+                    --per_sequence_function ${OUT_DIR}/KO_predicted.tsv.gz
+
+# add descriptions
+add_descriptions.py -i ${OUT_DIR}/KEGG_pathways_out_stratified/path_abun_unstrat.tsv.gz \
+                    -o ${OUT_DIR}/KEGG_pathways_out_stratified/path_abun_unstrat_descrip.tsv.gz \
+                    --custom_map_table ${KEGG_NAME}.tsv
+
+add_descriptions.py -i ${OUT_DIR}/KEGG_pathways_out_stratified/path_abun_unstrat_per_seq.tsv.gz \
+                    -o ${OUT_DIR}/KEGG_pathways_out_stratified/path_abun_unstrat_per_seq_descrip.tsv.gz \
+                    --custom_map_table ${KEGG_NAME}.tsv
+
+
+# unzip output contents for downstream analysis
+gunzip ${OUT_DIR}/KEGG_pathways_out_stratified/*.gz
+gunzip ${OUT_DIR}/KO_metagenome_out_stratified/*.gz
+
+sleep 60
+echo "Job ended"
+
+d2=$(date +%s) 
+sec=$(( ( $d2 - $d1 ) )) 
+hour=$(echo - | awk '{ print '$sec'/3600}') 
+echo Runtime: $hour hours \($sec\s\) 
--- README.md
+++ README.md
@@ -10,13 +10,12 @@ This repository contains all scripts used to analyze the Nepali oral microbiome.
 4. extraction_comparison - compare Qiagen and PowerSoil extraction kits
 5. microbiome_characterization - examine metrics for standard microbiome characteristics (alpha and beta diversity)
 6. random_forest - use Random Forests to predict lifestyle based on lifestyle survey data and microbiome data
-7. gut_rarefaction - rarefying the Nepal gut microbiome samples (from Jha et al., 2018) in prep for differential abundance analysis; uses phyloseq object acquired from paper
-8. differential_abundance - perform differential abundance analysis with ALDEx2 to identify taxa that differ based on lifestyle
-9. microbiome_trend - perform trend test on all genera to see which microbial abundances follow the lifestyle trend
-10. CCA - conduct CCA to identify which specific lifestyle factors played the greatest role in the microbiome
-11. Taxa_lifestyle - identify significant associations between specific lifestyle factors and DA microbes identified from the trend test.
-12. PICRUSt2_prep - prepping data for PICRUSt2
-13. PICRUSt2_stratified (shell) - run stratified version of PICRUST2 to predict pathway abundances.
-14. PICRUSt2_analysis - analyze PICRUSt2 output. All PICRUSt2 output from script 13 is assumed to be stored in the output directory in its own directory "PICRUSt2_result"
-15. network_analysis - conduct network analysis of the microbiome using SPARCC
-16. gut_oral_comparison - examine the relationship between the oral and gut microbiomes
+7. differential_abundance - perform differential abundance analysis with ALDEx2 to identify taxa that differ based on lifestyle
+8. microbiome_trend - perform trend test on all genera to see which microbial abundances follow the lifestyle trend
+9. CCA - conduct CCA to identify which specific lifestyle factors played the greatest role in the microbiome
+10. Taxa_lifestyle - identify significant associations between specific lifestyle factors and DA microbes identified from the trend test.
+11. PICRUSt2_prep - prepping data for PICRUSt2
+12. PICRUSt2_stratified (shell) - run stratified version of PICRUST2 to predict pathway abundances.
+13. PICRUSt2_analysis - analyze PICRUSt2 output. All PICRUSt2 output from script 13 is assumed to be stored in the output directory in its own directory "PICRUSt2_result"
+14. network_analysis - conduct network analysis of the microbiome using SPARCC
+15. gut_oral_comparison - examine the relationship between the oral and gut microbiomes
